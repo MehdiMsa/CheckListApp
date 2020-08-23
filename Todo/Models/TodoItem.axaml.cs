e0c98166c3b42cbc79c2116450f1c2d1368f6daa
MehdiMsa <mehdimsallem2906@gmail.com>
2020-08-23 15:31:20 +0100

    Version 1.0

---
+++ Todo/Models/TodoItem.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Todo.Models
+{
+    public class TodoItem
+    {
+        public string Description { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
